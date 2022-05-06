cd195cb987912f22bc924586e9ac5122c156c89a
Bryan da Silva Bruzinga <bryanbruzinga1990@gmail.com>
2022-05-06 10:23:44 -0300

    aplicação web

---
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -0,0 +1,25 @@
+import { FeedbackType, feedbackTypes } from "..";
+
+type Props = {
+  setFeedbackType: (arg: FeedbackType) => void;
+};
+
+export function FeedbackTypeStep({ setFeedbackType }: Props) {
+  return (
+    <div className="flex py-8 gap-2 w-full">
+      {Object.entries(feedbackTypes).map(([key, value]) => {
+        return (
+          <button
+            type="button"
+            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+            onClick={() => setFeedbackType(key as FeedbackType)}
+            key={key}
+          >
+            <img src={value.image.source} alt={value.image.alt} />
+            <span>{value.title}</span>
+          </button>
+        );
+      })}
+    </div>
+  );
+}
